5e0ba9e90cef2f4fb2fcfbc26e12e73613f0c31d
JoacoAba <joaco1130@hotmail.com>
2022-09-08 15:49:15 -0300

    HerenciaAballay added

--- My project15/Assets/Scripts/DamageItems.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DamageItems : MonoBehaviour
-{
-    [SerializeField][Range(1,50)]  int damage = 5;
-    public int Damage {get {return damage;}}
-}
